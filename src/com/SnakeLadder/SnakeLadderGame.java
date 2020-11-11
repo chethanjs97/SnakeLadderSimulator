2544c52b74b6b8d75477690651b04f6a3a8a075c
chethanjs97 <chethanjschethu@gmail.com>
2020-11-11 22:57:21 +0530

    Added : Check for Options NoPlay,Ladder,Snake

--- src/com/SnakeLadder/SnakeLadderGame.java
+++ src/com/SnakeLadder/SnakeLadderGame.java
@@ -1,15 +1,37 @@
 package com.SnakeLadder;
 
 public class SnakeLadderGame {
-	int position=0;
+	static int position=0;
 	public static void main(String[] args) {
 		System.out.println("Single Player at '0' Position");
 		dieRolls();
+		checkOption();
 		
 	}
-	public static void dieRolls() {
+	public static int dieRolls() {
 		int die=(int)(Math.floor(Math.random()*10)%6+1);
-		System.out.println("Dice = "+die);
+		System.out.println("Die = "+die);
+		return die;
+	}
+	public static void checkOption() {
+		int option=(int)(Math.floor(Math.random()*10)%3+1);
+		System.out.println("Option is "+option);
+		int die=dieRolls();
+		switch (option) {
+		case 1 :
+			System.out.println("No Play The Player Stays in Same Position  "+position);
+			break;
+		case 2 :
+			position=position+die;
+			System.out.println("Ladder the player moves ahead by position "+position);
+			break;
+		case 3 :
+			position=position-die;
+			System.out.println("Snake the player moves behind by position "+position);
+			break;
+			
+		}
+		
 	}
 
 }
