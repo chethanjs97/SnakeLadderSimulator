333343ab04128073d6c709ee01adddde6e2ce67f
chethanjs97 <chethanjschethu@gmail.com>
2020-11-11 23:17:55 +0530

    Added : Player Play Till Win

--- src/com/SnakeLadder/SnakeLadderGame.java
+++ src/com/SnakeLadder/SnakeLadderGame.java
@@ -4,7 +4,6 @@ public class SnakeLadderGame {
 	static int position=0;
 	public static void main(String[] args) {
 		System.out.println("Single Player at '0' Position");
-		dieRolls();
 		checkOption();
 		
 	}
@@ -14,6 +13,7 @@ public class SnakeLadderGame {
 		return die;
 	}
 	public static void checkOption() {
+		while (position<=100) {
 		int option=(int)(Math.floor(Math.random()*10)%3+1);
 		System.out.println("Option is "+option);
 		int die=dieRolls();
@@ -28,9 +28,13 @@ public class SnakeLadderGame {
 		case 3 :
 			position=position-die;
 			System.out.println("Snake the player moves behind by position "+position);
+			if(position<0) {
+				position=0;
+			}
 			break;
 			
 		}
+	}
 		
 	}
 
