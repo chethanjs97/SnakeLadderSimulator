c970b2562787b866e5ee6827c3ec686a0f6626e1
chethanjs97 <chethanjschethu@gmail.com>
2020-11-11 22:13:51 +0530

    Added : The Player rolls the die

--- src/com/SnakeLadder/SnakeLadderGame.java
+++ src/com/SnakeLadder/SnakeLadderGame.java
@@ -4,7 +4,13 @@ public class SnakeLadderGame {
 	int position=0;
 	public static void main(String[] args) {
 		System.out.println("Single Player at '0' Position");
+		dieRolls();
 		
+	}
+	public static void dieRolls() {
+		int die=(int)(Math.floor(Math.random()*10)%6+1);
+		System.out.println("Dice = "+die);
 	}
 
 }
+
