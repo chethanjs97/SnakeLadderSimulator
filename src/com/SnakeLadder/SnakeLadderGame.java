944f766efd1991421a96d5ab17be461c06a6a967
chethanjs97 <chethanjschethu@gmail.com>
2020-11-16 21:27:05 +0530

    Added : Two Players Playing

--- src/com/SnakeLadder/SnakeLadderGame.java
+++ src/com/SnakeLadder/SnakeLadderGame.java
@@ -3,17 +3,30 @@ package com.SnakeLadder;
 public class SnakeLadderGame {
 	static int position=0;
 	static int dieCount=0;
+	static int user_position1=0;
+	static int user_position2=0;
 	public static void main(String[] args) {
-		System.out.println("Single Player at '0' Position");
-		checkOption();
-		
+		System.out.println("Game With Two Players");
+		while(user_position1<100 && user_position2<100) 
+		{
+			user_position1=play(user_position1);
+			System.out.println("number of die roll "+dieCount+" position of player1: "+user_position1);
+			user_position2=play(user_position2);;	
+			System.out.println("number of die roll "+dieCount+" position of player2: "+user_position2);
+		}
+		if(user_position1==100)
+			System.out.println("player 1 is winner");
+		else
+			System.out.println("player2 is winner");
 	}
+	
 	public static int dieRolls() {
 		int die=(int)(Math.floor(Math.random()*10)%6+1);
 		System.out.println("Die = "+die);
 		return die;
 	}
-	public static void checkOption() {
+	public static int play(int pos) {
+		position=pos;
 		while (position<100) {
 		int option=(int)(Math.floor(Math.random()*10)%3+1);
 		System.out.println("Option is "+option);
@@ -28,6 +41,7 @@ public class SnakeLadderGame {
 				position=position-die;
 			}
 			System.out.println("Ladder the player moves ahead by position "+position);
+			
 			break;
 		case 3 :
 			position=position-die;
@@ -42,6 +56,8 @@ public class SnakeLadderGame {
 		System.out.println("Number of times the dice was played to win the game is = "+dieCount);
 		
 	}
+		return position;
+		
 		
 	}
 
